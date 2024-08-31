469bdae4a2f79f82a5d7d2778eff5c829ab4b2ef
Ahmed Baz <developer.baz@gmail.com>
2024-08-31 05:06:43 +0300

    configure tomcat

--- src/main/java/com/management/system/ManagementSystemApp.java
+++ src/main/java/com/management/system/ManagementSystemApp.java
@@ -2,14 +2,21 @@ package com.management.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class ManagementSystemApp {
+public class ManagementSystemApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ManagementSystemApp.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ManagementSystemApp.class);
     }
 
 }
