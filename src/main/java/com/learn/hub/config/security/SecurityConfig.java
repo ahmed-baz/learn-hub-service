5d738bcaaf97c279839e46401a3f7eb04d32b44b
Ahmed Baz <ahmismail.c@stc.com.sa>
2024-11-03 20:49:39 +0200

    - update pom - update SecurityConfig - update AppExceptionHandler - update CourseController authorization with PreAuthorize - add Dockerfile and docker-compose - update the project configurations

--- src/main/java/com/learn/hub/config/security/SecurityConfig.java
+++ src/main/java/com/learn/hub/config/security/SecurityConfig.java
@@ -1,15 +1,14 @@
 package com.learn.hub.config.security;
 
 
-import com.learn.hub.enums.UserRoleEnum;
 import com.learn.hub.filter.AuthFilter;
 import com.learn.hub.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -22,6 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final UserService userService;
@@ -36,12 +36,6 @@ public class SecurityConfig {
             "/webjars/**", "/swagger-resources/configuration/ui"
     };
 
-    private final String RESOURCE_COURSES_API = "/api/v1/courses";
-    private final String COURSE_API = "/api/v1/courses/{id}";
-    private final String REGISTER_COURSE_API = "/api/v1/courses/register";
-    private final String UNREGISTER_COURSE_API = "/api/v1/courses/unregister";
-    private final String SCHEDULE_COURSE_API = "/api/v1/courses/schedule";
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -50,15 +44,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         auth ->
                                 auth
-                                        .requestMatchers(HttpMethod.GET, RESOURCE_COURSES_API).hasAnyAuthority(UserRoleEnum.STUDENT.name(), UserRoleEnum.INSTRUCTOR.name())
-                                        .requestMatchers(HttpMethod.POST, REGISTER_COURSE_API).hasAnyAuthority(UserRoleEnum.STUDENT.name())
-                                        .requestMatchers(HttpMethod.POST, UNREGISTER_COURSE_API).hasAnyAuthority(UserRoleEnum.STUDENT.name())
-                                        .requestMatchers(HttpMethod.POST, RESOURCE_COURSES_API).hasAnyAuthority(UserRoleEnum.INSTRUCTOR.name())
-                                        .requestMatchers(HttpMethod.PUT, COURSE_API).hasAnyAuthority(UserRoleEnum.INSTRUCTOR.name())
-                                        .requestMatchers(HttpMethod.DELETE, COURSE_API).hasAnyAuthority(UserRoleEnum.INSTRUCTOR.name())
-                                        .requestMatchers(HttpMethod.GET, COURSE_API).hasAnyAuthority(UserRoleEnum.STUDENT.name(), UserRoleEnum.INSTRUCTOR.name())
-                                        .requestMatchers(HttpMethod.GET, SCHEDULE_COURSE_API).hasAuthority(UserRoleEnum.STUDENT.name())
-                                        .requestMatchers(PUBLIC_ENDPOINTS).permitAll().anyRequest().authenticated()
+                                        .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                                        .anyRequest().authenticated()
 
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
--- src/main/java/com/learn/hub/controller/CourseController.java
+++ src/main/java/com/learn/hub/controller/CourseController.java
@@ -7,9 +7,9 @@ import com.learn.hub.vo.Course;
 import com.learn.hub.vo.RegisterCourse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,42 +22,50 @@ public class CourseController {
     private final CourseService courseService;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR','STUDENT')")
     public AppResponse<List<Course>> getCourses() {
         return new AppResponse<>(courseService.getAllCourses());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR','STUDENT')")
     public AppResponse<Course> findCourse(@PathVariable Long id) {
         return new AppResponse<>(courseService.findCourse(id));
     }
 
     @PostMapping("/register")
+    @PreAuthorize("hasAnyRole('STUDENT')")
     public AppResponse<Course> registerCourse(@Valid @RequestBody RegisterCourse request) {
         return new AppResponse<>(courseService.registerCourse(request));
     }
 
     @PostMapping("/unregister")
+    @PreAuthorize("hasAnyRole('STUDENT')")
     public AppResponse<Long> unregisterCourse(@Valid @RequestBody RegisterCourse request) {
         return new AppResponse<>(courseService.unregisterCourse(request));
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR')")
     public AppResponse<Course> createCourse(@Valid @RequestBody Course course) {
         return new AppResponse<>(courseService.addCourse(course));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR')")
     public AppResponse<Course> updateCourse(@PathVariable Long id, @Valid @RequestBody Course course) {
         return new AppResponse<>(courseService.updateCourse(id, course));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR')")
     public AppResponse<Void> deleteCourse(@PathVariable Long id) {
         courseService.deleteCourse(id);
         return new AppResponse<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/schedule")
+    @PreAuthorize("hasAnyRole('STUDENT')")
     public ResponseEntity<byte[]> exportCourseSchedule() {
         return courseService.exportCourseSchedule();
     }
