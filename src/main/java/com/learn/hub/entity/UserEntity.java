ecedd8f715bd9e5a8d2be472849c91b258c15522
Ahmed Baz <ahmismail.c@stc.com.sa>
2024-11-03 22:04:40 +0200

    update UserEntity

--- src/main/java/com/learn/hub/entity/UserEntity.java
+++ src/main/java/com/learn/hub/entity/UserEntity.java
@@ -4,14 +4,20 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.learn.hub.enums.UserRoleEnum;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.security.Principal;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Data
 @Entity
 @Table(name = "users")
-public class UserEntity extends EntityBase {
+public class UserEntity extends EntityBase implements UserDetails, Principal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
@@ -34,4 +40,51 @@ public class UserEntity extends EntityBase {
             joinColumns = @JoinColumn(name = "student_id"),
             inverseJoinColumns = @JoinColumn(name = "course_id"))
     private Set<CourseEntity> courses = new HashSet<>();
+    private boolean accountLocked;
+    private boolean enabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+
+    public String fullName() {
+        return firstName + " " + lastName;
+    }
 }
