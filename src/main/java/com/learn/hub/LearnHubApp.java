1b061450a3558c67523efbd95282639857208018
Ahmed Baz <ahmedbaz1024@gmail.com>
2024-12-16 19:12:50 +0200

    setup eureka config

--- src/main/java/com/learn/hub/LearnHubApp.java
+++ src/main/java/com/learn/hub/LearnHubApp.java
@@ -3,11 +3,13 @@ package com.learn.hub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableCaching
 @EnableAsync
+@EnableDiscoveryClient
 public class LearnHubApp {
 
     public static void main(String[] args) {
